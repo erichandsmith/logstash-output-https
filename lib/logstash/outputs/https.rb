153726bf804a27cfc19c7f262414d32159b96846
Eric Hand-Smith <ehand-smith@emdeon.com>
2015-05-15 11:17:37 -0400

    Initial commit.

---
+++ lib/logstash/outputs/https.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+require "logstash/outputs/base"
+require "logstash/namespace"
+require "logstash/json"
+
+class LogStash::Outputs::Https < LogStash::Outputs::Base
+	config_name "https"
+
+    config :headers, :validate => :hash
+    config :url, :validate => :string, :required => :true
+    config :verb, :validate => ["put", "post"], :default => "post", :required => :true
+
+	public
+	def register
+        require "net/http"
+        require "uri"
+
+        @uri = URI.parse(@url)
+        @agent = Net::HTTP.new(@uri.host, @uri.port)
+        @agent.use_ssl = true
+        @agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	end
+
+	public
+	def receive(event)
+		return unless output?(event)
+
+        begin
+			case @verb
+				when "put"
+					request = Net::HTTP::Put.new(event.sprintf(@uri.request_uri))
+				when "post"
+					request = Net::HTTP::Post.new(event.sprintf(@uri.request_uri))
+				else
+					@logger.error("Unknown verb: ", :verb => @verb)
+			end
+
+			if @headers
+				@headers.each do |k,v|
+					request[k] = event.sprintf(v)
+				end
+			end
+
+			request["Content-Type"] = "application/json"
+			request.body = event.to_json_with_metadata
+
+            response = @agent.request(request)
+            #puts response.code
+
+        rescue Exception => e
+            @logger.warn("Unhandled exception: ", :request => request, :response => response, :exception => e, :stacktrace => e.backtrace)
+        end
+	end # def encode
+
+end # class LogStash::Outputs::Https
